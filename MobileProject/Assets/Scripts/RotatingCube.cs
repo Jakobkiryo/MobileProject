3b7b8c41a1a763a86c264209fd6cc47dd7ee6542
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-03-12 10:30:14 +0100

    Added the right scirpt that was shown

--- MobileProject/Assets/Scripts/RotatingCube.cs
+++ MobileProject/Assets/Scripts/RotatingCube.cs
@@ -1,18 +1,15 @@
-
 using UnityEngine;
 
-public class Rotating : MonoBehaviour
+public class RotatingCube : MonoBehaviour
 {
-    public Vector3 rotationAmount;
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("Hello World!");
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Rotate((rotationAmount * Time.deltaTime));
+        transform.Rotate(0, 90f*Time.fixedDeltaTime, 0);
+        Debug.Log("Next Step.");
     }
 }
