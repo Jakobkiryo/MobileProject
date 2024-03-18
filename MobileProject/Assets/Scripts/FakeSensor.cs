2eca1b466e169df3241809ffc751d72256d5bfc3
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-03-18 14:47:48 +0100

    Added the new input system to the project

---
+++ MobileProject/Assets/Scripts/FakeSensor.cs
@@ -0,0 +1,26 @@
+﻿namespace DefaultNamespace
+{
+    public struct Vector3Data
+    {
+        public float X, Y, Z;
+    }
+    
+    public class FakeSensor : System.Object
+    {
+        public static FakeSensor Instance { get; }
+        
+        public Vector3Data FakeStuff { get; }
+        
+        public int SampleRate { get; set; }
+
+        public void DoCoolStuff()
+        {
+            
+        }
+
+        public static void OtherCoolStuff()
+        {
+            
+        }
+    }
+}
---
+++ MobileProject/Assets/Scripts/SampleUsage.cs
@@ -0,0 +1,16 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+public class SampleUsage : MonoBehaviour
+{
+    void Start()
+    {
+        FakeSensor sensor = FakeSensor.Instance;
+        Vector3Data data = FakeSensor.Instance.FakeStuff;
+        Debug.Log(FakeSensor.Instance.SampleRate);
+        FakeSensor.Instance.SampleRate = 16;
+        
+        FakeSensor.OtherCoolStuff();
+        FakeSensor.Instance.DoCoolStuff();
+    }
+}
