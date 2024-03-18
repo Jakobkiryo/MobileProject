16be18d812bb8ff63dfed410542146b9a66a3faf
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-03-18 15:30:03 +0100

    Some changes

--- MobileProject/Assets/Scripts/FakeSensor.cs
+++ MobileProject/Assets/Scripts/FakeSensor.cs
@@ -1,26 +1,23 @@
-﻿namespace DefaultNamespace
+﻿public struct Vector3Data
 {
-    public struct Vector3Data
-    {
-        public float X, Y, Z;
-    }
+    public float X, Y, Z;
+}
     
-    public class FakeSensor : System.Object
+public class FakeSensor : System.Object
+{
+    public static FakeSensor Instance { get; }
+        
+    public Vector3Data FakeStuff { get; }
+        
+    public int SampleRate { get; set; }
+
+    public void DoCoolStuff()
     {
-        public static FakeSensor Instance { get; }
-        
-        public Vector3Data FakeStuff { get; }
-        
-        public int SampleRate { get; set; }
-
-        public void DoCoolStuff()
-        {
             
-        }
+    }
 
-        public static void OtherCoolStuff()
-        {
+    public static void OtherCoolStuff()
+    {
             
-        }
     }
 }
